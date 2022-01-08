3fde34b580d9b5d671faa92c8df72bc1868c760a
msudawg07 <93173601+msudawg07@users.noreply.github.com>
2022-01-07 21:45:44 -0600

    Update script.js

--- JSFundamentals-Part1/CodingChallenge2/script.js
+++ JSFundamentals-Part1/CodingChallenge2/script.js
@@ -1,1 +1,5 @@
-
+if(markBMI > johnBMI){
+  console.log(`Mark's BMI ${markBMI.toFixed(1)} is higher than John's`)
+}else{
+  console/log(`John's BMI ${johnkBMI.toFixed(1)} is higher than Mark's`)
+}
